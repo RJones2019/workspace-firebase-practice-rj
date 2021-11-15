e7d60ebde2be674b7d8b3b1a854cffc8e94d2be5
RJones2019 <rakeem@usca.edu>
2021-11-15 12:49:13 -0500

    1 file modified

--- js/Signup.js
+++ js/Signup.js
@@ -15,9 +15,9 @@ $("#signup-form").submit(function(e) {
   e.preventDefault();
   //get the username(email) and password from the form
   // change the following code
-  var email = $("#signup-form input [name='username']").val();
+  var email = $("#signup-form input[name='username']").val();
   console.log('email you typed is'+email);
-  var password = $("signup-form input [password='password']").val();
+  var password = $("signup-form input[password='password']").val();
 
   // create a user with email address and password
   firebase
