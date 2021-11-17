82b0a458b7067273890156a1ad26a0540e0e0a14
RJones2019 <rakeem@usca.edu>
2021-11-17 12:52:52 -0500

    1 file modified

--- js/SaveDisplay.js
+++ js/SaveDisplay.js
@@ -1,6 +1,14 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {};
+var firebaseConfig = {
+  apiKey: "AIzaSyBl7J7Wd1Zq6_oa10KIlBp7OO4R-K5ar-k",
+  authDomain: "csci225rj.firebaseapp.com",
+  projectId: "csci225rj",
+  storageBucket: "csci225rj.appspot.com",
+  messagingSenderId: "120469188494",
+  appId: "1:120469188494:web:7d80eaf13fd44518913edf",
+  measurementId: "G-N8W4M8HZVG"
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
@@ -8,11 +16,22 @@ firebase.initializeApp(firebaseConfig);
 $(".sampleSurvey input[type='submit']").click(function(e) {
   e.preventDefault();
 
-  // get the value of the form using serializeArray method
-  
+  var inputdata=$('.simpleSurvey').serializeArray();
+  inputdata.forEach((data)=>{
+  console.log(data.name);
+  console.log(data.value);
 
 
+var inputJson={};
+  for(var i=0;i<inputdata.length;i++){
+    var n = inputdata[i]["name"];
+    var v = inputdata[i]["value"];
+    inputJson[n]=v;
+    console.log(n+' '+v);
+  }
+  firebase.firestore().collection("surveydatabase").add(inputJson);
 
 });
-
+});
 // update the result in table
+
